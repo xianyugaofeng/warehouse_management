633b7132cbbddd37a113ab6cd3707d48e2384788
xianyugaofeng <zx1355458340@126.com>
2023-10-30 13:03:30 +0000

    update host_chatroom.py.
    Signed-off-by: xianyugaofeng <zx1355458340@126.com>

--- host_chatroom.py
+++ host_chatroom.py
@@ -7,6 +7,7 @@ class Host:
     def __init__(self):
         self.socket = None
         self.socketlist = []
+        self.user = {}
 
     def setup(self):
         host = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,11 +17,32 @@ class Host:
         host.listen(5)
         self.socket = host
 
-    def waitforacess(self):
+    def wait_for_access(self):
         while True:
             hostsocket, address = self.socket.accept()
             self.socketlist.append(hostsocket)
             print(f'有新的客户端{address}连接')
 
-    thread1 = threading.Thread(target=waitforacess)
-    thread1.start()
+    def broadcast(self):
+        sendmsg = '欢迎来到聊天室'.encode('utf-8')
+        for i in self.socketlist:
+            i.send(sendmsg)
+
+
+"""           
+root = Tk()
+root.title('host')
+root.resizable(True, False)
+root.config(bg="white")
+root.geometry("800x600")
+textframe = Frame(root, bg='black', height=200, width=200)
+textframe.pack(side='bottom')
+root.mainloop()
+"""
+
+host = Host()
+host.setup()
+thread1 = threading.Thread(target=host.wait_for_access)
+thread1.start()
+while True:
+    host.broadcast()
