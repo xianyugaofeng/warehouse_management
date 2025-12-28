725e07af0e4bec7f7cee33f0e731e316d8530dde
xianyugaofeng <zx1355458340@126.com>
2025-12-28 11:40:55 +0800

    Merge remote-tracking branch 'origin/master'

--- app/__init__.py
+++ app/__init__.py
@@ -4,3 +4,18 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from app.config import config
 
+# 初始化扩展
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'  # 登录跳转视图
+migrate = Migrate()
+
+def create_app(config_name='default'):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+
+    # 绑定扩展
+    db.init_app(app)
+    login_manager.init_app(app)
+    migrate.init_app(app, db)
+
