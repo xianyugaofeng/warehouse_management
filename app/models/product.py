bf23e055d780c38185a1c6abb28e5c5d2767a579
xianyugaofeng <zx1355458340@126.com>
2025-12-29 12:09:21 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ app/models/product.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from app import db
+
+
+class Category(db.Model):    # 产品分类
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unqiue=True, nullable=False)  # 分类名称
+    desc = db.Column(db.String(256))  # 分类描述
+    create_time = db.Column(db.Datetime, default=datetime.utcnow)
+    products = db.relationship('Product', backref='category', lazy='dynamic')
+
+
